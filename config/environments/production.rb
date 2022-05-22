23337d2c3e9c2d699baceca03d1790a2a181602e
齋藤　涼太￥ <ryota@saitouryoutanoMacBook-Pro.local>
2022-05-22 14:14:10 +0900

    微修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'blooming-eyrie-90060.herokuapp.com' }
 end
