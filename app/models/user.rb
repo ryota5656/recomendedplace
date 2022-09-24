35295701c5bb6db8c493238baa1fc1e0146ccc26
ryota5656 <kopernix5656@gmail.com>
2022-09-24 14:23:31 +0900

    ゲストログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
     end
   end
 end
